935e996ebb5b14cf07d11fe7a23fd9cefcf110fa
Benny Daon <bennydaon@gmail.com>
2014-04-16 11:54:46 +0300

    first version for drawing the writing

--- hasadnaLogo.js
+++ hasadnaLogo.js
@@ -1,15 +1,16 @@
 $('.hasadnaLogo').each(function () {
   var ctx = this.getContext('2d');
-  var r = this.width/2;
+  var r = this.width/3;
   var step = 0;
   var edge = Math.PI/10;  /* size of half a typical's line edge in radians */
   var base = edge*2;      /* size of half of the base in radians */
   var innerCircle = Math.ceil(r/3);  /* radius of the inner circle in pixels */
+
   ctx.lineWidth = Math.ceil(r/12);
 
  /* line and fill colors */
-  ctx.strokeStyle = "#00adef";  
-  ctx.fillStyle = "#00adef";  
+  ctx.strokeStyle = "#00adef";
+  ctx.fillStyle = "#00adef";
   /* move our center to the circle's center */
   ctx.save();
   ctx.translate(r,r);
@@ -46,5 +47,28 @@ $('.hasadnaLogo').each(function () {
   ctx.beginPath();
   ctx.arc(0,0, innerCircle, 0, Math.PI*2, false);
   ctx.fill();
+    ctx.font = '30pt Alef Hebrew';
+    ctx.textAlign = 'right';
+    ctx.lineWidth = 4;
+    drawTextAlongArc(ctx, 'הסדנא לידע ציבורי', 0, 0, r*1.5, edge*4);
+    ctx.stroke();
   ctx.restore();
 });
+  // for drawing the name stolen from
+  // http://www.html5canvastutorials.com/labs/html5-canvas-text-along-arc-path/
+  function drawTextAlongArc(context, str, centerX, centerY, radius, angle) {
+    var len = str.length, s;
+    context.save();
+    context.translate(centerX, centerY);
+    context.rotate(-1 * angle / 2);
+    context.rotate(-1 * (angle / len) / 2);
+    for(var n = len; n >= 0; n--) {
+      context.rotate(angle / len);
+      context.save();
+      context.translate(0, -1 * radius);
+      s = str[n];
+      context.fillText(s, 0, 0);
+      context.restore();
+    }
+    context.restore();
+  };
